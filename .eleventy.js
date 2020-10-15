11df531a58d7836a53c0206b29c4da7fd50c3dd5
Karim Daghari <karim_daghari@outlook.com>
2020-10-15 12:59:46 +0100

    .

--- .eleventy.js
+++ .eleventy.js
@@ -5,15 +5,16 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginSEO = require("eleventy-plugin-seo");
 
 module.exports = function (config) {
-  const markdownLib = markdownIt().use(markdownItFooter).use(markdownItSup);
+  // Global settings
+  config.setDataDeepMerge(true);
 
-  config.addPassthroughCopy([
-    "./src/favicon.ico",
-    "./src/images",
-    "./src/css/styles.css",
-    "./src/css/prism.css"
-  ]);
+  // Files needed as-is
+  config.addPassthroughCopy("./src/favicon.ico");
+  config.addPassthroughCopy("./src/images");
+  config.addPassthroughCopy("./src/css/prism.css");
+  config.addPassthroughCopy("./src/css/styles.css");
 
+  // Filters
   config.addFilter("prettyDate", function (value) {
     const data = new Date(value);
     return data.toLocaleDateString(undefined, {});
@@ -26,10 +27,13 @@ module.exports = function (config) {
       .sort((p1, p2) => p2.date - p1.date)
   );
 
+  // Plugins
   config.addPlugin(syntaxHighlight);
   config.addPlugin(pluginSEO, require("./src/_data/seo.json"));
+
+  // Library
+  const markdownLib = markdownIt().use(markdownItFooter).use(markdownItSup);
   config.setLibrary("md", markdownLib);
-  config.setDataDeepMerge(true);
 
   return {
     dir: {
