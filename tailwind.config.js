01b7fa28a10f0257d9c90e0d420b1aa3662d0b06
Karim Daghari <karim_daghari@outlook.com>
2020-10-19 21:48:10 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,45 +4,7 @@ module.exports = {
     content: ["./src/**/*.html", "./src/**/*.liquid"]
   },
   theme: {
-    extend: {
-      typography: theme => ({
-        default: {
-          css: {
-            color: theme("colors.gray.100"),
-            h1: {
-              color: theme("colors.gray.100")
-            },
-            h2: {
-              color: theme("colors.gray.100")
-            },
-            h3: {
-              color: theme("colors.gray.100")
-            },
-            h4: {
-              color: theme("colors.gray.100")
-            },
-            h5: {
-              color: theme("colors.gray.100")
-            },
-            h6: {
-              color: theme("colors.gray.100")
-            },
-            strong: {
-              color: theme("colors.gray.100")
-            },
-            a: {
-              color: theme("colors.gray.100")
-            },
-            blockquote: {
-              color: theme("colors.gray.400")
-            },
-            code: {
-              color: theme("colors.gray.200")
-            }
-          }
-        }
-      })
-    }
+    extend: {}
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
