cccc93d725c88c70a81f4d6f05531ec29dbc886c
Karim Daghari <karim_daghari@outlook.com>
2020-10-31 21:00:45 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,34 @@ module.exports = {
     content: ["./src/**/*.html", "./src/**/*.liquid"]
   },
   theme: {
+    typography: {
+      default: {
+        css: {
+          color: "#fff",
+          a: {
+            color: "#fff"
+          },
+          h1: {
+            color: "#fff"
+          },
+          h2: {
+            color: "#fff"
+          },
+          h3: {
+            color: "#fff"
+          },
+          h4: {
+            color: "#fff"
+          },
+          h5: {
+            color: "#fff"
+          },
+          h6: {
+            color: "#fff"
+          }
+        }
+      }
+    },
     extend: {}
   },
   variants: {},
